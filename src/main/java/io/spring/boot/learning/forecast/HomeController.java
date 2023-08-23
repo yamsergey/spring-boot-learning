ca974ef2663d6335b26c2830b254258cd60fd263
_HaDiaK_V_ <123464081+HadiakV@users.noreply.github.com>
2023-08-23 21:57:02 +0200

    Issue1 (#2)
    Implemented basic home screen
    ---------
    
    Co-authored-by: Olga Iamshchikova <oljabotova@gmail.com>, @HadiakV

--- src/main/java/io/spring/boot/learning/forecast/HomeController.java
+++ src/main/java/io/spring/boot/learning/forecast/HomeController.java
@@ -1,12 +1,21 @@
 package io.spring.boot.learning.forecast;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class HomeController {
-    @GetMapping("/")
+    @GetMapping(value = "/",produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
     public String home() {
-        return "Home page of the best forecast application ever.";
+        return """
+                <html>
+                <header><title>Welcome</title></header>
+                <body>
+                Home page of the best forecast application ever.
+                </body>
+                </html>""";
     }
 }
