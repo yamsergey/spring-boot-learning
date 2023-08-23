1aa4d95c846b2765bb09382a4bc2bb48697c59c5
HadiakV <jekoird@gmail.com>
2023-08-23 21:44:14 +0200

    Use proper content type

--- src/main/java/io/spring/boot/learning/forecast/HomeController.java
+++ src/main/java/io/spring/boot/learning/forecast/HomeController.java
@@ -1,14 +1,22 @@
 package io.spring.boot.learning.forecast;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class HomeController {
-    @GetMapping("/")
+    @GetMapping(value = "/",produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
     public String home() {
 //        return "Home page of the best forecast application ever.";
-        return "<html>\n" + "<header><title>Welcome</title></header>\n" +
-                "<body>\n" + "Home page of the best forecast application ever.\n" + "</body>\n" + "</html>";
+        return """
+                <html>
+                <header><title>Welcome</title></header>
+                <body>
+                Home page of the best forecast application ever.
+                </body>
+                </html>""";
     }
 }
